c1e780a3f8851648447a492bb696edeb3e1f399d
Sunghun Park <spark@jeenee.org.au>
2015-03-06 15:35:31 +1100

    first commit

---
+++ app/src/main/java/com/spark/app/ocb/entity/Practice.java
@@ -0,0 +1,30 @@
+package com.spark.app.ocb.entity;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * @author sunghun
+ *
+ */
+@DatabaseTable(tableName="practices")
+public class Practice{
+
+	@DatabaseField(generatedId=true) public int id = -1;
+	@DatabaseField public String dateTime;
+	@DatabaseField public int total;    //Total question number
+	@DatabaseField public int picked;	//Number of picked question
+	@DatabaseField public int correct;	//Number of right answer
+	
+	public Practice(){}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Practice [id=" + id + ", dateTime=" + dateTime + ", total="
+				+ total + ", picked=" + picked + ", correct=" + correct + "]";
+	}
+
+}

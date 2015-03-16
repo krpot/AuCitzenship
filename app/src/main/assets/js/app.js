19a73c6592d65272182bc7742984ed43b384a11d
Sunghun Park <spark@jeenee.org.au>
2015-03-16 17:07:14 +1100

    html resources added.

---
+++ app/src/main/assets/js/app.js
@@ -0,0 +1,77 @@
+/* app.js */
+var app = angular.module('ocb', [                                 	
+  'ui.router',
+  'ui.bootstrap'
+])
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+	//Configuration for route
+	$urlRouterProvider.otherwise("/part1"); //If no definition, go to /dashboard
+	
+	$stateProvider
+    .state('home', {
+		templateUrl: 'index.html',
+		controller : 'HomeController'
+    })
+    .state('home.part1', {
+    	url: '/part1',
+		templateUrl: '101.html',
+		controller : function(){}
+    })
+    .state('home.part2', {
+    	url: '/part2',
+		templateUrl: '201.html',
+		controller : function(){}
+    })
+    .state('home.part3', {
+    	url: '/part3',
+    	templateUrl: '301.html',
+		controller : function(){}
+    })
+    .state('home.page', {
+    	url: '/:page',
+    	templateUrl: function(stateParams){
+            return stateParams.page + '.html';
+        },
+		controller : function(){}
+    })
+})
+.controller('HomeController', function($scope){
+    console.log('======= HomeController created ==========');
+    $scope.menu = {
+        "part1": [
+            {id:101, title:"Our People"},
+            {id:102, title:"Australia's states and territories"},
+            {id:103, title:"Traditions and Symbols"},
+            {id:104, title:"Australia's flags"},
+            {id:105, title:"Australia's national anthem"}
+        ],
+        "part2": [
+            {id:201, title:"Our democratic beliefs"},
+            {id:202, title:"Our freedoms"},
+            {id:203, title:"Our equalities"},
+            {id:204, title:"Responsibilities and privileges of Australian citizenship"},
+            {id:205, title:"Our democratic beliefs"}
+        ],
+        "part3":[
+            {id:301, title:"How do I have my say?"},
+            {id:302, title:"How did we establish our system of government?"},
+            {id:303, title:"How is the power of government controlled?"},
+            {id:304, title:"Who is Australia’s Head of State?"},
+            {id:305, title:"Who are some of Australia’s leaders?"},
+            {id:306, title:"How is Australia governed?"},
+            {id:307, title:"What do the three levels of government do?"},
+            {id:308, title:"What role do political parties play in the way Australia is governed?"},
+            {id:309, title:"How are laws administered?"}
+        ]
+    };
+
+    $scope.currentKey;
+    $scope.setMenu = function(key){
+        $scope.currentKey = key;
+        $scope.currentMenu = $scope.menu[key];
+        $scope.navCollapsed = true;
+    }
+
+    $scope.setMenu('part1');
+})
